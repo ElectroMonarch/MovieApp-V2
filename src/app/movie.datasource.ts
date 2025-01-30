8d2b07a2ce5137f810fc24b97d7dea7179e3508a
Yağız Akın BAŞTOPÇU <ybastopcu@hotmail.com>
2025-01-30 12:11:51 +0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch version_0.3
    Your branch is up to date with 'origin/version_0.3'.
    
    Changes to be committed:
    	modified:   src/app/app.module.ts
    	new file:   src/app/movie-detail/movie-detail.component.css
    	new file:   src/app/movie-detail/movie-detail.component.html
    	new file:   src/app/movie-detail/movie-detail.component.spec.ts
    	new file:   src/app/movie-detail/movie-detail.component.ts
    	modified:   src/app/movie.datasource.ts
    	modified:   src/app/movies/movies.component.html

--- src/app/movie.datasource.ts
+++ src/app/movie.datasource.ts
@@ -1,9 +1,9 @@
-import {Movie} from './movie';
+import { Movie } from './movie';
 
 export const Movies: Movie[] = [
-    {id: 1, name:"movie 1"},
-    {id: 2, name:"movie 2"},
-    {id: 3, name:"movie 3"},
-    {id: 4, name:"movie 4"},
-    {id: 5, name:"movie 5"}
+    { id: 1, name: "Thor" },
+    { id: 2, name: "Godfather" },
+    { id: 3, name: "ElectroMonarch" },
+    { id: 4, name: "Kara Şövalye" },
+    { id: 5, name: "Inception" }
 ];

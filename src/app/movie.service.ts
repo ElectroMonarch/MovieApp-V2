82e63964119b0da34007586b1f5d0aa087442692
Yağız Akın BAŞTOPÇU <ybastopcu@hotmail.com>
2025-01-31 12:10:41 +0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch Version_1.0
    Changes to be committed:
    	modified:   src/app/dashboard/dashboard.component.ts
    	modified:   src/app/movie.service.ts
    	modified:   src/app/movies/movies.component.html
    	modified:   src/app/movies/movies.component.ts

--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -8,29 +8,38 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
-export class MovieService {   
+export class MovieService {
 
   private apiMoviesUrl = 'api/movies';
 
   constructor(
     private loggingService: LoggingService,
     private http: HttpClient
-    ) { }
+  ) { }
 
-  getMovies(): Observable<Movie[]> {    
+  getMovies(): Observable<Movie[]> {
     this.loggingService.add('MovieService: listing movies');
     return this.http.get<Movie[]>(this.apiMoviesUrl);
   }
 
   getMovie(id): Observable<Movie> {
-    this.loggingService.add('MovieService: get detail by id='+id)
-    return this.http.get<Movie>(this.apiMoviesUrl+'/'+id);
+    this.loggingService.add('MovieService: get detail by id=' + id)
+    return this.http.get<Movie>(this.apiMoviesUrl + '/' + id);
   }
 
-  update(movie: Movie):Observable<any> {
-    const httpOptions= {
-      headers: new HttpHeaders({'Content-Type': 'application/json'})
+  update(movie: Movie): Observable<any> {
+    const httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
     }
     return this.http.put(this.apiMoviesUrl, movie, httpOptions)
+  }
+
+  add(movie: Movie): Observable<Movie> {
+    return this.http.post<Movie>(this.apiMoviesUrl, movie);
+  }
+
+  delete(movie: Movie): Observable<Movie> {
+    return this.http.delete<Movie>(this.apiMoviesUrl + '/' + movie.id);
+
   }
 }
